fb3d64ab3532869af179d62f082a5f5d8fd6451c
sitota49 <se.sitota.ezra@gmail.com>
2020-07-26 18:07:57 +0300

    result detail

--- food/src/components/ResultsList.js
+++ food/src/components/ResultsList.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import {View, Text, StyleSheet, FlatList} from 'react-native';
+import ResultDetail from './ResultsDetail';
+import ResultsDeatil from './ResultsDetail';
 
 const ResultsList = ({title, results}) => {
     
     return (
-    <View >
+    <View style={styles.container} >
         <Text style={styles.title}>{title}</Text>
         <FlatList
             horizontal
             data={results}
             keyExtractor={(result)=>result.id}
             renderItem={({item})=>{
-                    return <Text>{item.name}</Text>;
+                    return <ResultsDeatil result={item}/>;
                 }
             }
         />
@@ -22,7 +24,12 @@ const ResultsList = ({title, results}) => {
 const styles= StyleSheet.create({
    title:{
        fontSize:18,
-       fontWeight:'bold'
+       fontWeight:'bold',
+       marginLeft:15,
+       marginBottom:5
+   },
+   container:{
+       marginBottom:10
    }
 });
 
