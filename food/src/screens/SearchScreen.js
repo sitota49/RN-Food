0bc578b91030f284da6541e6722c0b87866b12e3
sitota49 <se.sitota.ezra@gmail.com>
2020-07-26 17:41:18 +0300

    flatlist rendering

--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -1,27 +1,17 @@
 import React, {useState} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import SearchBar from '../components/SearchBar';
-import yelp from '../api/yelp'
+import useResults from '../hooks/useResults'
+import ResultsList from '../components/ResultsList'
 
 const SearchScreen = () => {
     const [term , setTerm]= useState('');
-    const [results, setResults]= useState([]);
-    const [errorMessage, setErrorMessage]=useState('');
-
+    const [searchApi,results,errorMessage]=useResults();
     
-    const searchApi=async(searchTerm)=>{
-        try{
-            const response = await yelp.get('/search', {
-                params:{
-                    limit:50,
-                    term: searchTerm,
-                    location: 'Chambersburg '
-                }
-            });
-            setResults(response.data.businesses);
-        }catch(err){
-            setErrorMessage('Something went wrong');
-        }
+    const filterResultsByPrice=(price)=>{
+        return results.filter(result =>{
+            return result.price === price;
+        });
     };
 
     return (<View>
@@ -31,6 +21,9 @@ const SearchScreen = () => {
      onTermSubmit={()=>searchApi(term)}/>
         {errorMessage ? <Text>{errorMessage}</Text> : null}
         <Text>Found {results.length} results</Text>
+        <ResultsList results={filterResultsByPrice('$')} title="Cost Effective"/>
+        <ResultsList results={filterResultsByPrice('$$')} title="Bit Pricier"/>
+        <ResultsList results={filterResultsByPrice('$$$')} title="Big spender"/>
     </View>);
 };
 
